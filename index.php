2ce99f33fa0efcb5692f1c3c651ba194924af799
Ranithreddy <120581287+ranithreddy@users.noreply.github.com>
2024-08-06 20:04:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+
+ </script>
+
+<script src="js/jquery.min.js"></script>
+
+</head>
+<body>
+<?php include('function.php'); ?>
+
+<?php include('top.php'); ?>
+<?php include('slider.php'); ?>
+     <div class="about section" id="section-2">
+	  <div class="about-head text-center">
+	  <h3>About Us</h3>
+	  </div>
+	   <div class="container">		  
+		 <div class="col-md-4 about-grids">
+			 <h4>Our Vision</h4>
+			 <p>It is important to take care of the patient, to be followed by the patient, but it will happen at such a time that there is a lot of work and pain. For to come to the smallest detail, no one should practice any kind of work unless he derives some benefit from it. Do not be angry with the pain in the reprimand in the pleasure he wants to be a hair from the pain in the hope that there is no breeding. Unless they are blinded by lust, they do not come forth; they are in fault who abandon their duties and soften their hearts, that is, their labors.</p>
+		 </div>
+		 <div class="col-md-4 about-grids grid2">
+			 <h4>Our Mission</h4>
+			 <p>It is important to take care of the patient, to be followed by the patient, but it will happen at such a time that there is a lot of work and pain. For to come to the smallest detail, no one should practice any kind of work unless he derives some benefit from it. Do not be angry with the pain in the reprimand in the pleasure he wants to be a hair from the pain in the hope that there is no breeding. Unless they are blinded by lust, they do not come forth; they are in fault who abandon their duties and soften their hearts, that is, their labors.</p>
+		 </div>
+		 <div class="col-md-4 about-grids">
+			 <h4>Safety Information</h4>
+			 <p>It is important to take care of the patient, to be followed by the patient, but it will happen at such a time that there is a lot of work and pain. For to come to the smallest detail, no one should practice any kind of work unless he derives some benefit from it. Do not be angry with the pain in the reprimand in the pleasure he wants to be a hair from the pain in the hope that there is no breeding. Unless they are blinded by lust, they do not come forth; they are in fault who abandon their duties and soften their hearts, that is, their labors.</p>
+		 </div>
+	 </div>
+</div>	
+<div  class="section" id="section-3">
+<div id="portfolio" class="portfolio">
+   <div class="top-tours-head text-center">
+		  <h3>Gallery</h3>		 
+		  </div>
+	      <ul id="filters" class="clearfix wow bounceIn" data-wow-delay="0.4s">
+			<li><span class="filter active" data-filter="app card icon logo fun">ALL</span></li>
+			<li><span class="filter" data-filter="app">Domestic Travel</span></li>
+			<li><span class="filter" data-filter="card">Foreign Travel</span></li>
+			<li><span class="filter" data-filter="icon">Short Date Tour</span></li>
+			<li><span class="filter" data-filter="fun">Long Date Tour</span></li>
+	        </ul>
+	     <div id="portfoliolist">
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t1.jpg" class="img-responsive" alt=""/></a>
+							</div>
+					</div>				
+					<div class="portfolio app mix_all" data-cat="app" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t2.jpg" class="img-responsive" alt=""/></a>
+                             </div>
+					</div>		
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t3.jpg" class="img-responsive" alt=""/></a>
+							
+					</div>
+					</div>				
+					<div class="portfolio icon mix_all" data-cat="icon" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t4.jpg" class="img-responsive" alt=""/></a>
+							 
+						</div>
+					</div>	
+					<div class="portfolio card mix_all" data-cat="card" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t5.jpg" class="img-responsive" alt=""/></a>
+							 
+						</div>
+					</div>			
+					<div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t6.jpg" class="img-responsive" alt=""/></a>
+							
+						</div>
+			      </div>
+				  <div class="portfolio fun mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+							<img src="images/t7.jpg" class="img-responsive" alt=""/></a>
+							
+						</div>
+			      </div>
+				  <div class="portfolio icon mix_all" data-cat="fun" style="display: inline-block; opacity: 1;">
+						<div class="portfolio-wrapper wow bounceIn" data-wow-delay="0.4s">		
+							<a href="#" class="b-link-stripe b-animate-go  thickbox play-icon popup-with-zoom-anim">
+						     <img src="images/t8.jpg" class="img-responsive" alt=""/></a>
+							 
+					   </div>
+			      </div>
+		   <div class="clearfix"></div>	
+	  </div>
+</div>
+</div>  
+<div id="section-5" class="contact section">
+	  <div class="contact-head text-center">
+		  <h3>Contact Us</h3>
+		  <br>
+		  <h4 style="color:#000">Plan Your Trip</h4>
+		  <br>
+		  <h5 style="color:#000">Our travel experts can help you book now</h5>
+
+		  <div class="contact-grids">
+			  <div class="container">
+				  <div class="col-md-4 address">
+						<h4 style="color:#09F"></h4>
+						<p style="color:#000"><br/>
+                        </p>
+                        <br/>
+                      </div>
+				  <div class="col-md-8 contact">
+                  
+                  	<?php
+if(isset($_POST["sbmt"]))
+{
+	$cn=makeconnection();
+	$s="insert into contactus(Name,Phno,Email,Message) values('" . $_POST["t1"] ."','" . $_POST["t2"] ."','" . $_POST["t3"] ."','" . $_POST["t4"] ."')";
+	mysqli_query($cn,$s);
+	mysqli_close($cn);
+	echo "<script>alert('Record Save');</script>";
+}
+?>
+					  <form method="post">
+                      <table border="0" width="700px" height="500px">
+                      <tr><td align="left"> <input type="text" class="text" value=" Name"  name="t1" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Name';}" required pattern="[a-zA-z1 _]{1,50}" title"Please Enter Only Characters and numbers between 1 to 50 for Name"></td></tr>
+                      <tr><td align="left"><input type="text" class="text" value=" Contact No" name="t2" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your Contact NO';}" required pattern="[0-9]{10,12}" title"Please Enter Only  numbers between 10 to 12 for Contact no"></td></tr>
+					 <tr><td align="left"> <input type="text" class="text" value="Email" name="t3" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = 'Your mail';}" required></td></tr>
+					 <tr><td><textarea onFocus="if(this.value == 'Message') this.value='';" name="t4" onBlur="if(this.value == '') this.value='Enter Message Here';" required/ >Message</textarea></td></tr>
+					  <tr><td><input type="submit" value="Send message" name="sbmt"></td></tr></table>
+					  <div class="clearfix"></div>
+					   </form>
+				   </div>
+				  <div class="clearfix"></div>
+			  </div>
+		  </div>
+<?php include('bottom.php'); ?>
+</body>
+</html>
+
